0a2683a00b6878ae0a3e9496d5b10a9cc00391a1
Zhuklevich Gleb <zhuklevich.gleb@gmail.com>
2026-04-05 22:08:43 +0300

    feat: lightcurve plot

--- phase/phase_curve.py
+++ phase/phase_curve.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import scienceplots
 
 
 def import_data(path: str):
@@ -10,7 +12,24 @@ def import_data(path: str):
 
     return data_vis
 
+def try_convert(x):
+    try:
+        return float(x)
+    except (ValueError, TypeError):
+        return np.nan
+    
+    
+
+def plot(data: pd.DataFrame):
+    fig, ax = plt.subplots()
+    date = data["JD"].to_numpy()
+    # m = data["Magnitude"].to_numpy(dtype=np.float16, na_value="-1", numeric_only=True)
+    m = pd.to_numeric(data["Magnitude"], errors="coerce")
+    ax.plot(date, m)
+    plt.show()
 
 if __name__ == "__main__":
     d_v = import_data("lightcurve.txt")
+    plot(d_v)
+    
     
