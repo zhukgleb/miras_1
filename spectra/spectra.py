2915905c82c817d9999fe89a7de38b7f47aaa132
Zhuklevich Gleb <zhuklevich.gleb@gmail.com>
2026-04-28 11:59:20 +0300

    feat: add barycentric correction

--- spectra/spectra.py
+++ spectra/spectra.py
@@ -3,20 +3,38 @@ import PyAstronomy.pyasl as pyasl
 from dech_processing import make_txt_from_spectra
 import matplotlib.pyplot as plt
 import scienceplots
+import os
 
 
-bcvr_arr = [8200.202]  # in ms
+dir_path = os.path.dirname(os.path.realpath(__file__))
+folder_to_spectra = dir_path + "/R_Cam/"
+spectra_content = os.listdir(folder_to_spectra)
+spectra_content.remove("20120413")
+spectra_path = [
+    folder_to_spectra + spectra_content[i] for i in range(len(spectra_content))
+]
 
-wf = "/home/alpha/miras_1/spectra/R_Cam/20111115/"
-rd = make_txt_from_spectra(wf, True, True)
+print(spectra_path)
+# rd = make_txt_from_spectra(wf, True, True)
 
 
-_, rd[:, 0] = pyasl.dopplerShift(
-    rd[:, 0], rd[:, 1], bcvr_arr[0] / 1000, edgeHandling="firstlast"
-)
+def test():
+    bcvr_arr = [
+        8200.185,
+        4450.928,
+        6698.503,
+        -4907.927,
+        -6186.043,
+        10429.766,
+        -10185.650,
+        5704.073,
+    ]
 
+    rd[:, 0] = pyasl.dopplerShift(
+        rd[:, 0], rd[:, 1], bcvr_arr[0] / 1000, edgeHandling="firstlast"
+    )
 
-with plt.style.context("science"):
-    fig, ax = plt.subplots()
-    ax.plot(rd[:, 0], rd[:, 1])
-    plt.show()
+    with plt.style.context("science"):
+        fig, ax = plt.subplots()
+        ax.plot(rd[:, 0], rd[:, 1])
+        plt.show()
