741cc0206b40fab0efc27b6622b8baf52c61370b
Zhuklevich Gleb <zhuklevich.gleb@gmail.com>
2026-04-28 20:54:54 +0300

    feat: now spectra module works fine, but ugly

--- spectra/spectra.py
+++ spectra/spectra.py
@@ -10,31 +10,37 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 folder_to_spectra = dir_path + "/R_Cam/"
 spectra_content = os.listdir(folder_to_spectra)
 spectra_content.remove("20120413")
+spectra_content.remove("20131008")
 spectra_path = [
-    folder_to_spectra + spectra_content[i] for i in range(len(spectra_content))
+    folder_to_spectra + spectra_content[i] + "/" for i in range(len(spectra_content))
 ]
 
 print(spectra_path)
+
+rd = []
+for i in range(len(spectra_path)):
+    rd.append(make_txt_from_spectra(spectra_path[i], True, True))
 # rd = make_txt_from_spectra(wf, True, True)
 
 
-def test():
-    bcvr_arr = [
-        8200.185,
-        4450.928,
-        6698.503,
-        -4907.927,
-        -6186.043,
-        10429.766,
-        -10185.650,
-        5704.073,
-    ]
+bcvr_arr = [
+    8200.185,
+    4450.928,
+    6698.503,
+    -4907.927,
+    -6186.043,
+    10429.766,
+    -10185.650,
+    5704.073,
+]
 
-    rd[:, 0] = pyasl.dopplerShift(
-        rd[:, 0], rd[:, 1], bcvr_arr[0] / 1000, edgeHandling="firstlast"
+for i in range(len(rd)):
+    _, rd[i][:, 0] = pyasl.dopplerShift(
+        rd[i][:, 0], rd[i][:, 1], bcvr_arr[i] / 1000, edgeHandling="firstlast"
     )
 
-    with plt.style.context("science"):
-        fig, ax = plt.subplots()
-        ax.plot(rd[:, 0], rd[:, 1])
-        plt.show()
+with plt.style.context("science"):
+    fig, ax = plt.subplots()
+    for i in range(len(rd)):
+        ax.plot(rd[i][:, 0], rd[i][:, 1])
+    plt.show()
