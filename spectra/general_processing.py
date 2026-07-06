23dac8810b845483163bf620fa6d7d6fc788d6f7
Zhuklevich Gleb <zhuklevich.gleb@gmail.com>
2026-07-06 13:25:51 +0300

    feat: add median normalization

--- spectra/general_processing.py
+++ spectra/general_processing.py
@@ -12,6 +12,8 @@ def get_spectra_cut(start_wl, end_wl, data):
     return cut_data[0]
 
 
+
+# For future usage
 def detect_orders(data):
     orders = []
     orders_gaps_idx = np.where(data[:, 1] == 0)
@@ -21,3 +23,40 @@ def detect_orders(data):
     plt.show()
     return orders
 
+def median_normalization(data):
+    wavelengths = data[:, 0]
+    flux = data[:, 1].copy() 
+    
+    zero_mask = (flux == 0)
+    zero_indices = np.where(zero_mask)[0]
+    
+    if len(zero_indices) == 0:
+        mask = (flux != 0)
+        if np.any(mask):
+            median_val = np.median(flux[mask])
+            if median_val != 0:
+                flux[mask] /= median_val
+        return np.column_stack((wavelengths, flux))
+    
+
+    start_idx = 0
+    for i, zero_idx in enumerate(zero_indices):
+        order_slice = flux[start_idx:zero_idx]
+        
+        order_mask = (order_slice != 0)
+        if np.any(order_mask):
+            median_val = np.median(order_slice[order_mask])
+            if median_val != 0:
+                flux[start_idx:zero_idx][order_mask] /= median_val
+        
+        start_idx = zero_idx + 1
+    
+    if start_idx < len(flux):
+        order_slice = flux[start_idx:]
+        order_mask = (order_slice != 0)
+        if np.any(order_mask):
+            median_val = np.median(order_slice[order_mask])
+            if median_val != 0:
+                flux[start_idx:][order_mask] /= median_val
+    
+    return np.column_stack((wavelengths, flux))
